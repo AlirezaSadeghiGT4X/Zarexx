569055232778c8b665fb8a9c02eca545743b3388
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-18 23:17:37 +0330

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Page from './Components/Page';
+
 export default function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Page />} path={"/"} />
+      </Routes>
+    </Router>
   )
 }
