54e042eecebf8d82daab698422060e7a1bd30f87
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-20 22:00:28 +0330

    Fix scroll bug in safari

--- src/Components/Page.tsx
+++ src/Components/Page.tsx
@@ -2,8 +2,8 @@ import Header from "./Header/Header";
 
 export default function Page() {
     return (
-        <>
+        <section className="scroll-smooth">
             <Header />
-        </>
+        </section>
     )
 }
