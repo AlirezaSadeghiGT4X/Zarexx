4a53c5bd36286118f93a0557e7e98d089890a310
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-20 14:56:15 +0330

    Add logo to header

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
-        <div>
-            
+        <div className="w-30 ml-4 md:ml-20 lg:ml-24 sm:w-46 md:w-52 lg:w-58">
+            <img src="/public/Images/Logo.png" alt="Zarexx" />
         </div>
     )
 }
