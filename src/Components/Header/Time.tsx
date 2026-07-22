faaa3952e81a02af0fae1fde201d84d95208dee6
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-22 23:11:36 +0330

    Add show day of the week feature

--- src/Components/Header/Time.tsx
+++ src/Components/Header/Time.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/set-state-in-effect */
 import { format } from "date-fns-jalali";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaNumbers } from "../PublicComponents/FaNumbers";
 
 export default function Time() {
 	let time = new Date();
+	const [dayOfTheWeek, setDayOfTheWeek] = useState<string>("");
 	const [hour, setHour] = useState<string>(FaNumbers(Nicing("hour")));
 	const [minute, setMinute] = useState<string>(FaNumbers(Nicing("minute")));
 	const [second, setSecond] = useState<string>(FaNumbers(Nicing("second")));
@@ -36,9 +39,35 @@ export default function Time() {
 		setSecond(FaNumbers(Nicing("second")));
 	}, 100);
 	const shamsiDate: string = format(time, "yyyy/MM/dd");
+	//*Get day of the week and translate to persian
+	useEffect(() => {
+		const day: number = time.getDay();
+		if (day == 0) {
+			setDayOfTheWeek("یکشنبه");
+		}
+		if (day == 1) {
+			setDayOfTheWeek("دوشنبه");
+		}
+		if (day == 2) {
+			setDayOfTheWeek("سه‌شنبه");
+		}
+		if (day == 3) {
+			setDayOfTheWeek("چهارشنبه");
+		}
+		if (day == 4) {
+			setDayOfTheWeek("پنجشنبه");
+		}
+		if (day == 5) {
+			setDayOfTheWeek("جمعه");
+		}
+		if (day == 6) {
+			setDayOfTheWeek("شنبه");
+		}
+	}, [time.getDay()]);
 	return (
 		<div className="flex flex-col text-zinc-900 dark:text-zinc-200 text-xs lg:text-sm md:flex-row gap-0.5 md:gap-2 items-center justify-center">
 			<span>{FaNumbers(shamsiDate)}</span>
+			<span>{dayOfTheWeek}</span>
 			<span className="hidden md:block">●</span>
 			<span>
 				{hour} : {minute} : {second}
