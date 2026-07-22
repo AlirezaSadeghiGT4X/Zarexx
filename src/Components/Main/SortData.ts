598111070cb5233a2801868a1750b898a15b6cdd
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-23 00:56:04 +0330

    Change assets places in UI

--- src/Components/Main/SortData.ts
+++ src/Components/Main/SortData.ts
@@ -8,13 +8,6 @@ export function SortData(data: Data) {
         const favorites: Array<Asset> = []
         if (data.gold.length > 0) {
             favorites.push({
-                id: 1,
-                name: "طلا ۱۸ عیار",
-                price: parseInt(data.gold[0].price),
-                changeValue: data.gold[0].change_value,
-                changePercent: data.gold[0].change_percent,
-            })
-            favorites.push({
                 id: 2,
                 name: "انس طلا",
                 price: parseInt(data.gold[3].price),
@@ -22,11 +15,11 @@ export function SortData(data: Data) {
                 changePercent: data.gold[3].change_percent,
             })
             favorites.push({
-                id: 3,
-                name: "یورو",
-                price: parseInt(data.currency[2].price),
-                changeValue: data.currency[2].change_value,
-                changePercent: data.currency[2].change_percent,
+                id: 1,
+                name: "طلا ۱۸ عیار",
+                price: parseInt(data.gold[0].price),
+                changeValue: data.gold[0].change_value,
+                changePercent: data.gold[0].change_percent,
             })
             favorites.push({
                 id: 4,
@@ -36,20 +29,6 @@ export function SortData(data: Data) {
                 changePercent: data.currency[0].change_percent,
             })
             favorites.push({
-                id: 5,
-                name: "ربع سکه",
-                price: parseInt(data.gold[5].price),
-                changeValue: data.gold[5].change_value,
-                changePercent: data.gold[5].change_percent,
-            })
-            favorites.push({
-                id: 6,
-                name: "سکه گرمی",
-                price: parseInt(data.gold[4].price),
-                changeValue: data.gold[4].change_value,
-                changePercent: data.gold[4].change_percent,
-            })
-            favorites.push({
                 id: 7,
                 name: "سکه امامی",
                 price: parseInt(data.gold[7].price),
@@ -57,11 +36,18 @@ export function SortData(data: Data) {
                 changePercent: data.gold[7].change_percent,
             })
             favorites.push({
-                id: 8,
-                name: "نیم سکه",
-                price: parseInt(data.gold[6].price),
-                changeValue: data.gold[6].change_value,
-                changePercent: data.gold[6].change_percent,
+                id: 3,
+                name: "یورو",
+                price: parseInt(data.currency[2].price),
+                changeValue: data.currency[2].change_value,
+                changePercent: data.currency[2].change_percent,
+            })
+            favorites.push({
+                id: 10,
+                name: "سکه بهارآزادی",
+                price: parseInt(data.gold[8].price),
+                changeValue: data.gold[8].change_value,
+                changePercent: data.gold[8].change_percent,
             })
             favorites.push({
                 id: 9,
@@ -71,11 +57,25 @@ export function SortData(data: Data) {
                 changePercent: data.cryptocurrency[0].change_percent,
             })
             favorites.push({
-                id: 10,
-                name: "سکه بهارآزادی",
-                price: parseInt(data.gold[8].price),
-                changeValue: data.gold[8].change_value,
-                changePercent: data.gold[8].change_percent,
+                id: 8,
+                name: "نیم سکه",
+                price: parseInt(data.gold[6].price),
+                changeValue: data.gold[6].change_value,
+                changePercent: data.gold[6].change_percent,
+            })
+            favorites.push({
+                id: 6,
+                name: "سکه گرمی",
+                price: parseInt(data.gold[4].price),
+                changeValue: data.gold[4].change_value,
+                changePercent: data.gold[4].change_percent,
+            })
+            favorites.push({
+                id: 5,
+                name: "ربع سکه",
+                price: parseInt(data.gold[5].price),
+                changeValue: data.gold[5].change_value,
+                changePercent: data.gold[5].change_percent,
             })
             setAssets(favorites)
         }
