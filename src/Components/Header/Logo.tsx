f85236c8435f3d1b1a2766604b7b52c4e3332591
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-07-24 01:30:04 +0330

    Set connection strings for deploy

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <div className="w-30 sm:w-46 md:w-52 lg:w-58 absolute left-1/2 -translate-x-1/2">
-            <img src="/public/Images/Logo.png" alt="Zarexx" />
+            <img src="/Images/Logo.png" alt="Zarexx" />
         </div>
     )
 }
--- src/Components/Main/Loading.tsx
+++ src/Components/Main/Loading.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export default function Loading(): ReactNode {
 	return (
 		<video autoPlay muted loop playsInline width="120" height="120">
-			<source src="/public/Animations/Loading.webm" type="video/webm"></source>
+			<source src="/Animations/Loading.webm" type="video/webm"></source>
 		</video>
 	);
 }
