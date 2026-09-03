77ef797d8c06ba03a0dd4d9cd8223b539ac5ff26
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-09-03 17:31:25 +0330

    Add Yektanet for advertisement managing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Page from './Components/Page';
-import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Page from "./Components/Page";
+import NotFound from "./Components/NotFound/NotFound";
+import Yektanet from "./Components/Advertisement/Yektanet";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route element={<Page />} path={"/"} />
-        <Route element={<NotFound />} path={"*"} />
-      </Routes>
-    </Router>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route element={<Page />} path={"/"} />
+				<Route element={<NotFound />} path={"*"} />
+			</Routes>
+			<Yektanet />
+		</Router>
+	);
 }
---
+++ src/Components/Advertisement/Yektanet.tsx
@@ -0,0 +1,5 @@
+export default function Yektanet(){
+    return(
+        <div id="pos-article-display-122651"></div>
+    )
+}
