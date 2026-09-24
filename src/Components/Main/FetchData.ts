9813ad37cfb4a2544189dc2ed7ef73b0901501b7
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-09-25 00:24:35 +0330

    Set secrets in env file

--- src/Components/Main/FetchData.ts
+++ src/Components/Main/FetchData.ts
@@ -2,7 +2,7 @@ import type { Data } from "./Types/Types"
 
 export async function FetchData() {
     try {
-        const fetchedData = await fetch("https://api.brsapi.ir/Market/Gold_Currency.php?key=B6XD3NY59A9eN7HhIphJAcv8A35tLA6f")
+        const fetchedData = await fetch(import.meta.env.VITE_PRICES_API_LINK)
         if (!fetchedData.ok) {
             throw new Error("Failed to catch data")
         }
