b1508141cddf480c5e01d71134d778a02d73ca21
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-09-25 15:44:56 +0330

    Edit capacitor for debuging on phone

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ir.zarexx.app',
   appName: 'Zarexx',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    webContentsDebuggingEnabled: true,
+  },
 };
 
 export default config;
